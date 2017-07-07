c3bfd92e72ce537999544d731a3679c85350b5b9
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2017-07-07 17:34:43 -0400

    Adding a Logger

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,10 +1,14 @@
 defmodule Issues.GithubIssues do
+
+  require Logger
+
   @user_agent  [ {"User-agent", "Elixir dave@pragprog.com"} ]
 
   # use a module attribute to fetch the value at compile time
   @github_url Application.get_env(:issues, :github_url)
 
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
     issues_url(user, project)
     |> Tesla.get(headers: @user_agent)
     |> handle_response
@@ -15,10 +19,13 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response(%{status: 200, body: body}) do
+    Logger.info "Successful response"
+    Logger.debug fn -> inspect(body) end
     { :ok,    Poison.Parser.parse!(body) }
   end
 
-  def handle_response(%{status: _,   body: body}) do
+  def handle_response(%{status: status,   body: body}) do
+    Logger.error "Error #{status} returned"
     { :error, Poison.Parser.parse!(body) }
   end
 end
